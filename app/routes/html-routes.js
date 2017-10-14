b9ada9a29d7dcd8a6558c6bd918d30578579d973
Firas AlOmairi <firas.lcc@gmail.com>
2017-10-14 13:47:02 -0500

    using battle.ejs deleting batle.html no batle.html

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -3,13 +3,9 @@ module.exports = function(App) {
 	App.get("/", function(req, res) {
 		res.sendFile(Path.join(__dirname, "../public/index.html"));
 	});
-	// App.get("/battle", function(req, res) {
-	// 	res.sendFile(Path.join(__dirname, "../public/battle.html"));
-	// });
-	App.get("/newTrainer", function(req, res) {
-		res.sendFile(Path.join(__dirname, "../public/newTrainer.html"));
+	App.get("/battle", function(req, res) {
+		res.sendFile(Path.join(__dirname, "../public/battle.html"));
 	});
-	App.get("/viewTrainer", function(req, res) {
-		res.sendFile(Path.join(__dirname, "../public/viewTrainer.html"));
-	});
+	
+	
 }
--- app/routes/pokemon-api-routes.js
+++ app/routes/pokemon-api-routes.js
@@ -4,6 +4,7 @@ var Pokemon = require("../controllers/pokemon.js");
 module.exports = function(App) {
 	App.get("/battle/:trainerId?", function(req, res) {
 		console.log("HI");
+		res.render("battle.ejs")
 		var trainerId = req.params.trainerId;
 		if (trainerId) {
 			db.Trainer.findOne({
