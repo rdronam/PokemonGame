0c5542a4059f83cbb88d57f8f8bd5be52b470f34
Jason McKinney <meter_1@yahoo.com>
2017-10-15 20:38:33 -0500

    changed trainers-api to render both using identical object structures

--- app/routes/trainers-api-routes.js
+++ app/routes/trainers-api-routes.js
@@ -39,7 +39,7 @@ module.exports = function (App) {
 							exp: poke_Exp
 						};
 						var renderObj = poke;
-						res.render("battle.ejs",{{profile:renderObj});
+						res.render("battle.ejs",{profile:renderObj});
 					});
 				} else {
 					//SQL TO INSERT AND RENDER
@@ -52,6 +52,30 @@ module.exports = function (App) {
 						health_points_max: 200,
 						attack: 50,
 						experience: 0
+					}).then(function (dbTrainer){
+						var trainerInfo = dbTrainer.dataValues;
+						db.Pokemon.findOne({
+							where: {
+								number: randomPokemon
+							}
+						}).then(function (dbPokemon) {
+							var train_Name = trainerInfo.name;
+							var poke_Name = dbPokemon.dataValues.name;
+							var poke_Level = trainerInfo.level;
+							var poke_HP_C = trainerInfo.health_points_current;
+							var poke_Atk = trainerInfo.attack;
+							var poke_Exp = trainerInfo.experience;
+							var poke = {
+								name: train_Name,
+								pokemon: poke_Name,
+								level: poke_Level,
+								hp_c: poke_HP_C,
+								atk: poke_Atk,
+								exp: poke_Exp
+							};
+							var renderObj = poke;
+							res.render("battle.ejs",{profile:renderObj});
+						});
 					});
 				}
 			});
