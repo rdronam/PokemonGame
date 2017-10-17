39d5ddb908ea9d4d7499b134a93589a5397ea3c7
Firas AlOmairi <firas.lcc@gmail.com>
2017-10-16 23:23:52 -0500

    changing the form we get request

---
+++ app/public/js/battle.js
@@ -0,0 +1,11 @@
+$.post(currentURL + "/api/", userData, function(data){          
+$("#matchName").text(data.name);
+$('#matchImg').attr("src", data.photo);
+
+
+$(document).ready(function() {
+	$("#walkInGrass").on("click", generateBattle);
+	function generateBattle(event) {
+		event.preventDefault();
+
+$.get("/battle/1")
